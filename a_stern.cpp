7bf1611b18e325349ae25a366986a7503d6af338
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-15 20:36:30 +0200

    DIJKSTRA

--- a_stern.cpp
+++ a_stern.cpp
@@ -11,9 +11,45 @@ public:
     
     CostT cost( VertexT from, VertexT to) const override;
 };
+//#########################################################################################################
+//                              Hilfsfunktionen
+//#########################################################################################################
+
+bool allTrue( vector <int> S){
+    for( int i = 0; i < S.size() ; i++) if (S[i] ==0) return false;
+    return true;
+}
+int minIndex( vector< CostT> D){
+    int res = 0;
+    CostT min = infty;
+    for ( int i = 0; i < D.size(); i++) if (D[i]< min && D[i]!=0 ) res = i;
+    return i;
+}
+//#########################################################################################################
 
 void Dijkstra(const DistanceGraph& g, GraphVisualizer& v, VertexT start, std::vector<CostT>& kostenZumStart) {
     // ...
+    size_t n =g.numVertices();
+
+    vector< size_t > S(n,0) ;
+    vector< double > D(n,infty);
+    D[start] = 0;
+    S[start] = 1;
+    //erste Schleife
+    for ( int i = 0; i < n ; i++)
+        if( i != start)
+            D[i] = g(start,i);
+    while( !(allTrue(S)))
+    {
+        size_t v1 = minIndex(D);
+        S[v1] = 1; //fuege v1 zu S hinzu.
+        for ( int i = 0; i < n ; i ++)  if ( S[i] == 0)
+                                        if( (D[v1]+g(v1,i)) < D[i])
+                                            D[i] := D[v1]+g(v1,i);
+    }
+    
+    
+    
 }
 
 bool A_star(const DistanceGraph& g, GraphVisualizer& v, VertexT start, VertexT ziel, std::list<VertexT>& weg) {
