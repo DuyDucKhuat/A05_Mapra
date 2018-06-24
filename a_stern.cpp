5fd76e140fc7e38b50e5be44e25576161cb8d9e0
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-24 16:09:46 +0200

    maze

--- a_stern.cpp
+++ a_stern.cpp
@@ -118,7 +118,7 @@ void readMaze( std::ifstream& in , size_t rows, size_t cols, maze& G){
         }
     }
 }
-vod displayMaze (maze& G)
+void displayMaze (maze& G)
 {
     for ( int i = 0 ; i < G.rows; i ++){
         for ( int j = 0; j < G.cols ; j++){
@@ -158,14 +158,14 @@ int main()
     maze G2(breite,hoehe);
     readMaze(fin2, breite, hoehe, G2);
     std::vector<CostT> D2(breite*hoehe,infty);
-    //displayMaze(G2);
-    for ( int i = 0 ; i < G2.rows; i ++){
+    displayMaze(G2);
+    /*for ( int i = 0 ; i < G2.rows; i ++){
         for ( int j = 0; j < G2.cols ; j++){
             if( G(i,j) == CellType::Wall) std::cout << '#' << " ";
             else std::cout << ' ' << " ";
         }
         std::cout << "\n";
-    }
+    }*/
     Dijkstra(G2,0,D2);
     std::cout << G2.rows*G2.cols << std::endl;
     //PruefeDijkstra(1, 0, D2);
