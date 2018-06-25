2146f43886243049ebf80a25f335795aad53bc3a
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-25 16:25:53 +0200

    A_Stern

--- a_stern.cpp
+++ a_stern.cpp
@@ -125,7 +125,7 @@ bool A_star(const DistanceGraph& G, VertexT start, VertexT ziel, std::list<Verte
                 // evtl. neu
             // sind die neu?
         
-            NeighborT& N = G.getNeighbors(current);
+            NeighborT N = G.getNeighbors(current);
             for ( auto v : N ){
                 if ( !bekannt[v.first] ){
 
@@ -153,12 +153,10 @@ bool A_star(const DistanceGraph& G, VertexT start, VertexT ziel, std::list<Verte
             if( k > 376 ) std::cout << "hier5!!" << std::endl;
 
             if( queue.empty()){
-                delete &N;
                 return false;
             }
             k++;
             std::cout << k << std::endl;
-            delete &N;
             }
     return false; // Kein Weg gefunden.
     
