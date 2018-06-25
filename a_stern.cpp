103165da5bb0a29d4e33d6901a3d34c375d4fedc
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-25 18:22:16 +0200

    A_Stern

--- a_stern.cpp
+++ a_stern.cpp
@@ -304,6 +304,8 @@ int main(int argc, char * argv[])
         std::list < VertexT > weg;
         std::cout << "hi" << std::endl;
         std::vector < std::pair < VertexT, VertexT> > Aufgabe = StartZielPaare(1);
+        std::cout << "hi2" << std::endl;
+
         std::cout << Aufgabe[bsp2].first << " "<< Aufgabe[bsp2].second << std::endl;
         if( A_star(G, Aufgabe[bsp2].first, Aufgabe[bsp2].second, weg))
         {
