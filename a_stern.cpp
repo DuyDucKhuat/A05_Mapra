5dffab08eac1dc354c0eec6ac053593ee9841661
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-25 15:15:05 +0200

    A_Stern

--- a_stern.cpp
+++ a_stern.cpp
@@ -102,7 +102,7 @@ bool A_star(const DistanceGraph& G, VertexT start, VertexT ziel, std::list<Verte
         }
     int k = 0;
         while( true){
-            std::sort_heap(queue.begin(), queue.end(), compare());
+            std::pop_heap(queue.begin(),queue.end(),compare());
 
             current = queue.back().first;
             std::pop_heap(queue.begin(),queue.end(),compare());
