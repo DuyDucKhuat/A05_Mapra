54855e1b7814efb752d704f2d2464035142ea270
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-25 12:53:08 +0200

    A_Stern

--- a_stern.cpp
+++ a_stern.cpp
@@ -105,7 +105,7 @@ bool A_star(const DistanceGraph& G, VertexT start, VertexT ziel, std::list<Verte
 
             std::pop_heap(queue.begin(),queue.end(),compare());
             current = queue.back().first;
-            if(k > 163 ) std::cout << " current: "<<current << " " << queue.back().second << std::endl;
+            if(k > 163 ) std::cout << " current: "<<G.RowCol(current).first << " " <<G.RowCol(current).second << std::endl;
 
             queue.pop_back();
             // sind wir schon am Ziel?
