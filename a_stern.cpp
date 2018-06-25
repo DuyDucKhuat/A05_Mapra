a59dd5daee2dac898fef2a6ab473dded31e5d29d
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-25 11:40:05 +0200

    A_Stern

--- a_stern.cpp
+++ a_stern.cpp
@@ -267,7 +267,7 @@ int main()
     
     
     std::list<VertexT> weg2;
-    if(A_star(G2,9,14,weg2)) std::cout << "hi2 " << std::endl;
+    if(A_star(G2,9,6*8+4,weg2)) std::cout << "hi2 " << std::endl;
     for( auto v : weg2) std::cout << v << " ";
     std::cout << "\n";
     
