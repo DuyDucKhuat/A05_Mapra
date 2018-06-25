475d9039219bfa773bec7096c44e343bc62495dc
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-25 12:16:38 +0200

    A_Stern

--- a_stern.cpp
+++ a_stern.cpp
@@ -284,6 +284,7 @@ int main()
     displayMaze(G3);
     std::vector < std::pair < VertexT, VertexT> > Aufgabe = StartZielPaare(8);
     for ( auto v : Aufgabe){
+        std::cout << G3.RowCol(v.first).first << "  " << G3.RowCol(v.first).second << std::endl;
         std::cout << v.first << " " << v.second << std::endl;
         if( A_star(G3, v.first, v.second, weg3))
         {
