f7c554d6577b70da305feeec02e3c2a4ebea0da6
DuyDucKhuat <duc.khuat@hotmail.de>
2018-06-25 09:50:20 +0200

    A_Stern

--- a_stern.cpp
+++ a_stern.cpp
@@ -231,7 +231,7 @@ int main()
     
     std::list<VertexT> weg;
     if(A_star(G, 0, 8, weg)) std::cout << " hi" << std::endl;
-    PruefeWeg(2, weg);
+    PruefeWeg(3, weg);
     
     fin.close();
     // Lade die zugehoerige Textdatei in einen Graphen
