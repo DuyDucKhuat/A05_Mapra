c3cd534a0efbc1d56e583843180b0b9bbc1c19be
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-07 17:09:45 +0200

    test

--- Visualizer.cpp/MazeVisualizer.h
+++ Visualizer.cpp/MazeVisualizer.h
@@ -44,6 +44,7 @@ class MazeVisualizer : public GraphVisualizer
             }
             while (window.isOpen())
             {
+                sf::Event event;
                 while(window.pollEvent(event))
                 {
                     if (event.type == sf::Event::Closed)
