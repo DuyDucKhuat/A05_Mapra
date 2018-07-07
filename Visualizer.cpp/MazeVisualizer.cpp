0766f5c07455afaa090c0934277c544862d1253e
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-07 17:26:12 +0200

    test

--- Visualizer.cpp/MazeVisualizer.cpp
+++ Visualizer.cpp/MazeVisualizer.cpp
@@ -14,6 +14,24 @@ void MazeVisualizer::draw() {}
 
 int main (){
     MazeVisualizer test(80,60);
-    std::cout << test.widthRec << " " << test.heightRec << std::endl;
+    while (test.window.isOpen())
+    {
+        sf::Event event;
+        while(test.window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                test.window.close();
+        }
+        for (sf::Vector2f x : test.vertices ){
+            test.window.clear();
+            sf::RectangleShape rec( sf::Vector2f(widthRec,heightRec)  );
+            rec.setFillColor (BLUE);
+            rec.setOutlineThickness(1);
+            rec.setOutlineColor (BLACK);
+            rec.setPosition(x);
+            test.window.draw(rec);
+            test.window.display();
+        }
+    }
     return 0 ;
 }
