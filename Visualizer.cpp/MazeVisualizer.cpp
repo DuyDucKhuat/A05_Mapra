6459e0bb068567a45ee8c0fde8d0f9e3d353d8ed
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-08 12:20:43 +0200

    test

--- Visualizer.cpp/MazeVisualizer.cpp
+++ Visualizer.cpp/MazeVisualizer.cpp
@@ -33,7 +33,27 @@ int main (){
             test.window.display();
         }
     }*/
-    std::cout << " hi" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
     
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event)) // event loop
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        sf::CircleShape shape(200);
+        shape.setFillColor(sf::Color(50, 50, 30));
+        shape.setOutlineThickness(1); // creates an outline around the circle
+        shape.setOutlineColor(sf::Color(250, 150, 100)); // outline color is orange
+        shape.setOrigin(shape.getRadius(), shape.getRadius());
+        shape.setPosition(0,0); // positioning the circle 200 units right from the top left corner
+        window.draw(shape); // draws the shape, a green circle with an orange outline
+        window.display();
+    }
+
     return 0 ;
 }
