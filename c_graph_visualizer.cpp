857e8daef6c9da0c80f88c200266abcfbcec3b51
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-09 21:38:33 +0200

    maze Visualizer

--- c_graph_visualizer.cpp
+++ c_graph_visualizer.cpp
@@ -8,11 +8,11 @@
 #include "c_graph_visualizer.h"
 
 void c_graph_visualizer::draw(){
-   /* this->window->clear();
+    this->window->clear();
     //Circle
     sf::CircleShape shape(20);
     shape.setFillColor(sf::Color(50, 50,30));
     shape.setOutlineThickness(0.5);
-    shape.setOutlineColor(sf::Color(250,150,100));*/
+    shape.setOutlineColor(sf::Color(250,150,100));
     
 }
