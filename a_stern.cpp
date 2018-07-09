9fc5979c8bda8c9a25a8ee1294a0a73422e95aa9
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-09 14:44:03 +0200

    maze Visualizer

--- a_stern.cpp
+++ a_stern.cpp
@@ -99,7 +99,7 @@ bool A_star(const DistanceGraph& G,GraphVisualizer& V, VertexT start, VertexT zi
         std::push_heap(queue.begin(), queue.end(), compare());
         //aktualisiere
         bekannt[v.first] = true;
-        V.markVertex(,v.first, VertexStatus::InQueue);   //########NEU
+        V.markVertex(v.first, VertexStatus::InQueue);   //########NEU
         Vorgaenger[v.first] = start;
         V.markEdge(EdgeT (start,v.first),EdgeStatus::Active);      //########NEU
     }
@@ -107,7 +107,7 @@ bool A_star(const DistanceGraph& G,GraphVisualizer& V, VertexT start, VertexT zi
         std::pop_heap(queue.begin(),queue.end(),compare());
         current = queue.back().first;
         currentEdge.second = current;               //########NEU
-        V.markVertex( current ,VertexStatus::Visited);           //########NEU
+        V.markVertex( current ,VertexStatus::Active);           //########NEU
         V.markEdge( EdgeT (currentEdge.first,currentEdge.second),EdgeStatus::Visited); //########NEU
             
 
