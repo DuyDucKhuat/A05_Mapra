64416c4ea2733b388784c9c0b63dbb3daa684907
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-09 15:43:11 +0200

    maze Visualizer

--- MazeVisualizer.h
+++ MazeVisualizer.h
@@ -33,9 +33,9 @@ class MazeVisualizer : public GraphVisualizer
     MazeVisualizer(sf::RenderWindow* window1, maze& m1 ) :m(m1)
         {
             this->window = window1;
-            this->widthRec = 800./(this->m->cols);
-            this->heightRec = 600./(this->m->rows);
-            vertex_status.resize( this->m->numVertices(), VertexStatus::UnknownVertex);
+            this->widthRec = 800./(this->m.cols);
+            this->heightRec = 600./(this->m.rows);
+            vertex_status.resize( this->m.numVertices(), VertexStatus::UnknownVertex);
         }
         ~MazeVisualizer() {}
         void markVertex(VertexT vertex, VertexStatus status) override;
