1eb055a990552dbbd263a53ddced2160456d0f3a
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-09 23:37:50 +0200

    maze Visualizer

--- c_graph_visualizer.cpp
+++ c_graph_visualizer.cpp
@@ -19,7 +19,7 @@ void c_graph_visualizer::draw(){
     shape.setOutlineColor(sf::Color::Black);
     
     for ( int i  = 0 ; i < n ; i ++){
-        if(this->vertex_status[i] == VertexStatus::UnknownVesrtex) shape.setFillColor(sf::Color::Green);
+        if(this->vertex_status[i] == VertexStatus::UnknownVertex) shape.setFillColor(sf::Color::Green);
         else if(this->vertex_status[i] == VertexStatus::InQueue) shape.setFillColor(sf::Color::Yellow);
         else if(this->vertex_status[i] == VertexStatus::Done) shape.setFillColor(sf::Color::Cyan);
         else if(this->vertex_status[i] == VertexStatus::Active) shape.setFillColor(sf::Color::Red);
