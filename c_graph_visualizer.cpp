2f5eb21549d4b2657c152c504b0b28d3ef9afc35
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-10 09:30:18 +0200

    maze Visualizer

--- c_graph_visualizer.cpp
+++ c_graph_visualizer.cpp
@@ -34,7 +34,7 @@ void c_graph_visualizer::draw(){
     NeighborT N = G.getNeighbors(0);
     for (auto v :N){
         sf::Vertex line[] ={sf::Vertex(sf::Vector2f(shape.getPosition()), sf::Color::Black),
-            sf::Vertex(sf::Vector2f(400 + 20*(G.Koordinaten[v.first].first - x) , 300+ 20*(G.Koordinaten[v.first].second - y)), sf::Color::Black) };
+            sf::Vertex(sf::Vector2f(400 + 50*(G.Koordinaten[v.first].first - x) , 300+ 50*(G.Koordinaten[v.first].second - y)), sf::Color::Black) };
         window->draw(line, 2 , sf::Lines);
     }
     for ( int i  = 1 ; i < n ; i ++){
@@ -44,12 +44,12 @@ void c_graph_visualizer::draw(){
         else if(this->vertex_status[i] == VertexStatus::Active) shape.setFillColor(sf::Color::Red);
         else if(this->vertex_status[i] == VertexStatus::Destination) shape.setFillColor(sf::Color::Magenta);
         
-        shape.setPosition( 400 + 20*(G.Koordinaten[i].first - x) ,300 + 20*(G.Koordinaten[i].second - y) );
+        shape.setPosition( 400 + 50*(G.Koordinaten[i].first - x) ,300 + 50*(G.Koordinaten[i].second - y) );
         this->window->draw(shape);
         NeighborT N = G.getNeighbors(i);
         for (auto v :N){
             sf::Vertex line[] ={sf::Vertex(sf::Vector2f(shape.getPosition()), sf::Color::Black),
-            sf::Vertex(sf::Vector2f(400 +  20*(G.Koordinaten[v.first].first - x),300 + 20*(G.Koordinaten[v.first].second-y)), sf::Color::Black) };
+            sf::Vertex(sf::Vector2f(400 +  50*(G.Koordinaten[v.first].first - x),300 + 50*(G.Koordinaten[v.first].second-y)), sf::Color::Black) };
             window->draw(line, 2 , sf::Lines);
         }
     }
