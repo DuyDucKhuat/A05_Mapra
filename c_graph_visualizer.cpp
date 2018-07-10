500824be29d42ee3feaea0d30b52687b31cc3f74
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-10 22:43:20 +0200

    visualizer

--- c_graph_visualizer.cpp
+++ c_graph_visualizer.cpp
@@ -70,8 +70,8 @@ void c_graph_visualizer::draw(){
             std::string str3 = strs3.str();
             Kantengewicht.setString("Gewicht: " + str +  " (" + str2 + ", " + str3 + ")");
             Kantengewicht.setFillColor(sf::Color::Black);
-            Knoten.setOutlineColor( sf::Color::Black);
-            Knoten.setOutlineThickness(1);
+            Kantengewicht.setOutlineColor( sf::Color::Blue);
+            Kantengewicht.setOutlineThickness(0.5);
 
 
            // float MittelpunktX = sf::Vector2f(shape.getPosition()).x +  Skalierung*(G.Koordinaten[v.first].first - x);
