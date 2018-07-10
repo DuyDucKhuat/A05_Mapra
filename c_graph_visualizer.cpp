687cefb13dd0df1d7be0c5d1f31e35e8e5bef206
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-10 21:18:22 +0200

    maze Visualizer

--- c_graph_visualizer.cpp
+++ c_graph_visualizer.cpp
@@ -13,6 +13,7 @@ void c_graph_visualizer::draw(){
     
     sf::Text text;
     text.setFont(this-> font);
+    text.setCharacterSize(20);
     while(!weiter){
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) weiter = true;
 
@@ -22,7 +23,7 @@ void c_graph_visualizer::draw(){
     }
     this->window->clear(sf::Color::White);
     int Anfangsknoten = 22;  //Bsp .3 : Anfangsknoten 0, Bsp.4 Anfangsknoten z.b. 22
-    double Skalierung = 50; //Bsp .3 : Skalierung 10, Bsp.4 Skalierung 50
+    double Skalierung = 80; //Bsp .3 : Skalierung 10, Bsp.4 Skalierung 50
     double x = G.Koordinaten[Anfangsknoten].first; //Mittelpunkt
     double y = G.Koordinaten[Anfangsknoten].second;
     
