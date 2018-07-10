f28ee80d7eb5cba0fcd7c887af3874ee8344d241
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-10 22:56:02 +0200

    visualizer

--- c_graph_visualizer.cpp
+++ c_graph_visualizer.cpp
@@ -80,7 +80,7 @@ void c_graph_visualizer::draw(){
 
            // float MittelpunktX = sf::Vector2f(shape.getPosition()).x +  Skalierung*(G.Koordinaten[v.first].first - x);
            // float MittelpunktY = sf::Vector2f(shape.getPosition()).y +  Skalierung*(G.Koordinaten[v.first].second-y); //für das Kantengewicht
-            triangle.setPosition(sf::Vector2f((Skalierung-10)*(P.x + P2.x)/Skalierung, (Skalierung-10)*(P.y + P2.y)/Skalierung);
+            triangle.setPosition(sf::Vector2f((Skalierung-10)*(P.x + P2.x)/Skalierung, (Skalierung-10)*(P.y + P2.y)/Skalierung));
             Kantengewicht.setPosition(sf::Vector2f((P.x + P2.x)/2., (P.y + P2.y)/2.));
             window->draw(Kantengewicht);
             if( this->edge_status[i * n + v.first] ==EdgeStatus::UnknownEdge)
