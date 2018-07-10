2d561cda6daa53abf1438ef55a04b77728298eee
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-10 23:05:26 +0200

    visualizer

--- Visualizer.cpp/test_b/sfml_test.cpp
+++ Visualizer.cpp/test_b/sfml_test.cpp
@@ -41,7 +41,7 @@ int main()
         //triangle.setOrigin(triangle.getRadius(),triangle.getRadius());
         triangle.setPosition(400,300);
         //triangle.setOrigin(triangle.getRadius(),triangle.getRadius());
-        triangle.setRotation(23);
+        triangle.setRotation(0);
 
         // set the shape color to green
         shape.setFillColor(sf::Color(100, 250, 50));
