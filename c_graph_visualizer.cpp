7c853eaf2fa8f7206c6e47017a23de76f150b901
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-11 14:18:36 +0200

    visualizer

--- c_graph_visualizer.cpp
+++ c_graph_visualizer.cpp
@@ -25,7 +25,7 @@ void c_graph_visualizer::draw(){
             this->window->close();
     }
         this->window->clear(sf::Color::White); // BSP 2: Anfangsknoten 4, Skalierung 190
-    int Anfangsknoten = 22;  //Bsp .3 : Anfangsknoten 0, Bsp.4 Anfangsknoten z.b. 22
+    int Anfangsknoten = 7;  //Bsp .3 : Anfangsknoten 0, Bsp.4 Anfangsknoten z.b. 22
     double Skalierung = 55; //Bsp .3 : Skalierung 10, Bsp.4 Skalierung 50
     double x = G.Koordinaten[Anfangsknoten].first; //Mittelpunkt
     double y = G.Koordinaten[Anfangsknoten].second;
