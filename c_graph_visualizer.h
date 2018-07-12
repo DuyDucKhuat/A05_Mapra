f7e89e60ca8f030d38419079d61ad6b3e7427d6c
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-12 19:15:20 +0200

    visualizer

--- c_graph_visualizer.h
+++ c_graph_visualizer.h
@@ -28,8 +28,8 @@ class c_graph_visualizer : public GraphVisualizer
     CoordinateGraph G;
     std::vector<VertexStatus> vertex_status;
     std::vector<EdgeStatus> edge_status;
-    std::vector<CostT> Kosten;
-    std::vector<CostT> Heuristik;
+    //std::vector<CostT> Kosten;
+    //std::vector<CostT> Heuristik;
     int n;
     
     
@@ -42,16 +42,16 @@ class c_graph_visualizer : public GraphVisualizer
 
         vertex_status.resize( n, VertexStatus::UnknownVertex);
         edge_status.resize( n * n, EdgeStatus::UnknownEdge);
-        Kosten.resize(n, 0);
-        Heuristik.resize(n, 0);
+        //Kosten.resize(n, 0);
+        //Heuristik.resize(n, 0);
     }
     ~c_graph_visualizer(){}
     void markVertex(VertexT vertex, VertexStatus status) override {this->vertex_status[vertex] = status;}
     void markEdge(EdgeT e, EdgeStatus status) override {this->edge_status[e.first*n + e.second] = status;}
     void updateVertex(VertexT vertex,  double cost, double estimate, VertexT parent, VertexStatus status) override{
-        this->Kosten[vertex] = cost;
-        this->Heuristik[vertex] = estimate;
-        this->vertex_status[vertex] = status;
+        //this->Kosten[vertex] = cost;
+        //this->Heuristik[vertex] = estimate;
+        //this->vertex_status[vertex] = status;
     }
     void draw() override;
 };
