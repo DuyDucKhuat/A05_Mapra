3cff176fbf106e4b4cfe35a40d66195dc518241d
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-12 22:19:40 +0200

    visualizer

--- a_stern.cpp
+++ a_stern.cpp
@@ -75,7 +75,8 @@ bool A_star(const DistanceGraph& G,GraphVisualizer& V, VertexT start, VertexT zi
 
     static std::vector<CostT> Weglaenge(n, infty); // Vom Startknoten aus.
 
-    struct compare { // f =  g + h;
+    class compare { // f =  g + h;
+    public:
         bool operator () (std::pair<size_t , CostT> a, std::pair<size_t , CostT> b) const {
             return  Weglaenge[a.first]+ a.second > Weglaenge[b.first] + b.second;
         }
