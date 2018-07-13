42adf1de90a1deb3e454c0e99bb179523672b5e0
DuyDucKhuat <duc.khuat@hotmail.de>
2018-07-13 09:26:49 +0200

    visualizer

--- c_graph_visualizer.h
+++ c_graph_visualizer.h
@@ -50,7 +50,7 @@ class c_graph_visualizer : public GraphVisualizer
         //Heuristik.resize(n, 0);
         switch (Bsp){
             case 1:
-                this->Skalierung = 20000;
+                this->Skalierung = 50000;
                 this->Mittelpunkt = 3 ;
                 this->SkalierungText = 0.0002;
                 this->SkalierungDreieck = 0.0001;
